d536e7fd2382ca1224f58f0635c0fa41814510ff
Thomas Cook <tlncook85@gmail.com>
2022-06-14 21:30:09 -0600

    commit tree e63263d3cec7aae45317fef0787c4e441979b32d

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
+
+  function addStockToPortfolio(newStock) {
+    const isInPortfolio = portfolioStocks.some(
+      (stock) => stock.id === newStock.id
+    );
+    if (!isInPortfolio) {
+      setPortfolio((currentStatePortfolio) => [
+        ...currentStatePortfolio,
+        newStock,
+      ]);
+    }
+  }
+
+  function removeStockFromPortfolio(stockToRemove) {
+    setPortfolio((currentStocks) => 
+    currentStocks.filter((stock) => stock.id !== stockToRemove.id)
+    );
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer addStockRoPortfolio={addStockToPortfolio} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer 
+            stocks={portfolio}
+            removeStockFromPortfolio={removeStockFromPortfolio}
+          />
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ addStockToPortfolio }) {
+   const [stocks, setStocks] = useState([]);
+
+useEffect(() => {
+  fetch("http://localhost:3001/stocks")
+  .then(response => response.json())
+  .then((data) => setStocks(data));
+}, [])
+
+const renderStocks = stocks.map((stock) => (
+  <stock
+    key={stock.id}
+    stock={stock}
+    handleStockclick={addStockToPortfolio}
+  />
+));
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {renderStocks}
     </div>
   );
 }
